247e32aa2642b344b53f34d5c2a433de6659c300
ahmadwardak <ahmad.wardak@tum.de>
2020-06-15 16:57:33 +0200

    updated the config for cloud mongodb

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@
 const port = process.env.PORT || '3000';
 const socketPort = '3002';
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/echefdb';
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://admin:masterkey@cluster0-ozdd2.mongodb.net/echef?retryWrites=true&w=majority';//'mongodb://localhost:27017/echefdb';
 
 const JwtSecret = process.env.JWT_SECRET || 'Secret0fEcH3f';
 
