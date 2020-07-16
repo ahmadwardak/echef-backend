ec8b348293dce2f60c9a4f88df72ef387aa9862d
ahmadwardak <ahmad.wardak@tum.de>
2020-07-16 10:52:26 +0200

    Code Refactor

--- src/api.js
+++ src/api.js
@@ -12,6 +12,13 @@ const middlewares = require('./middlewares/middlewares');
 const auth = require('./routes/authRoute');
 const review = require('./routes/recipeReviewRoute');
 const ingredients = require('./routes/ingredientRoute');
+
+const recipe = require('./routes/recipeRoute');
+const category = require('./routes/categoryRoute');
+const levels = require('./routes/difficultyLevels');
+const order = require('./routes/orderRoute');
+const shoppingCart = require('./routes/shoppingCartRoute');
+
 // Morgan allows to log Http requests
 const morgan = require('morgan');
 morgan.token('body', (req, res) => {
@@ -20,13 +27,6 @@ morgan.token('body', (req, res) => {
 morgan.token('head', (req, res) => {
     return "Head of request:" + JSON.stringify(req.method)// req.body.name + req.body.number
 })
-
-const recipe = require('./routes/recipeRoute');
-const category = require('./routes/categoryRoute');
-const levels = require('./routes/difficultyLevels');
-const order = require('./routes/orderRoute');
-const shoppingCart = require('./routes/shoppingCartRoute');
-
 const api = express();
 
 
@@ -34,28 +34,15 @@ const api = express();
 api.use(helmet());
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: false }));
-
 api.use(middlewares.allowCrossDomainRequests);
 api.use(cors());
 
+api.use(morgan(':method :url :status :res[content-length] - :response-time ms - :head - :body '));
 
-api.use(morgan(':method :url :status :res[content-length] - :response-time ms - :head - :body '))
-
-
-
-// Basic route
-api.get('/', (req, res) => {
-    res.json({
-        name: 'eChef Backend'
-    });
-});
-
-
-let server = require('http').createServer(api).listen(parseInt(config.socketPort), () => {
-    console.log(`Socket.io is running in port ${config.socketPort}`);
+let server = require('http').createServer(api).listen(() => {
     console.log("Db connected at", config.mongoURI)
 });
-require('./routes/socket.js').initialize(server);
+
 
 api.use(morgan('dev'));
 // API routes
@@ -70,14 +57,13 @@ api.use('/levels', levels);
 api.use('/order', order);
 api.use('/shoppingCart', shoppingCart);
 
-
 //Static files
-api.use('/', express.static('public'));
 api.use('/public/uploads/reviews', express.static('public/uploads/reviews'));
 api.use('/public/uploads/recipes', express.static('public/uploads/recipes'));
 
 //use morgan here to handle not found error for route not found..... 
 api.use((req, res, next) => {
+    console.log('a');
     const error = new Error('Not Found');
     error.status = 404;
     next(error);
